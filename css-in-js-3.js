8ddac006e1df3b8a8f39f1eaede74bf505430123
white <exwhite@naver.com>
2023-01-14 22:53:23 +0900

    Update Momentum

---
+++ css-in-js-3.js
@@ -0,0 +1,11 @@
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+  const clickedClass = "clicked sext-font";
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.contains(clickedClass);
+  } else {
+    h1.classList.add(clickedClass);
+  }
+}
+h1.addEventListener("click", handleTitleClick);
