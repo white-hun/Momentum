ea86381378e203b1b63648249c0e03c719a48e41
white <exwhite@naver.com>
2023-01-14 21:09:41 +0900

    Update Momentum

---
+++ css-in-js.js
@@ -0,0 +1,13 @@
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    currentColor = "blue";
+  }
+  h1.style.color = newColor;
+}
+h1.addEventListener("click", handleTitleClick);
