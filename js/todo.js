03553f1e774fdbf31d16a63a0ea7c2f623ab87b7
white <exwhite@naver.com>
2023-01-16 23:49:48 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -13,14 +13,16 @@ function saveToDo() {
 
 function deleteToDo(event) {
   // click event로 delete함수 실행
-  const li = event.target.parentElement; // event로 click된 button의 부모 element 변수화 *(변수 li)
+  const li = event.target.parentElement; // event로 click된 button의 부모 element 변수화 *(변수 li);
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
   li.remove(); // 변수li 삭제
 }
 
-function paintToDo(newToDo) {
-  const li = document.createElement("li"); // html 내에 li element 생성 *(변수 li)
+function paintToDo(newTodo) {
+  const li = document.createElement("li"); // html 내에 li element 생성 *(변수 li);
+  li.id = newTodo.id;
   const span = document.createElement("span"); // html 내에 span element 생성 *(변수 span);
-  span.innerText = newToDo; // 변수span 내부 text는 handleToDoSubmit 함수로 받은 저장된 입력값;
+  span.innerText = newTodo.text; // 변수span 내부 text는 handleToDoSubmit 함수로 받은 저장된 입력값;
   const button = document.createElement("button"); // html 내에 button 생성 *(변수 button)
   button.innerText = "❌"; // button 내부 text X이모지로 변경
   button.addEventListener("click", deleteToDo); // click 시 deleteToDo함수 실행되는 event 생성
@@ -32,10 +34,14 @@ function paintToDo(newToDo) {
 function handleToDoSubmit(event) {
   // event 발생을 argument로 함수 실행
   event.preventDefault(); // 이벤트 발생 시 기본동작 통제;
-  const newToDo = toDoInput.value; // 변수toDoInput으로 입력되는 value 저장(변수화) *(변수 newToDo)
+  const newTodo = toDoInput.value; // 변수toDoInput으로 입력되는 value 저장(변수화) *(변수 newToDo)
   toDoInput.value = ""; // 입력칸 빈칸으로 만듬
-  toDos.push(newToDo); // 변수newToDo의 입력값을 변수toDos array에 저장
-  paintToDo(newToDo); // 입력값으로 paintToDo 함수 실행
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj); // 변수newToDo의 입력값을 변수toDos array에 저장
+  paintToDo(newTodoObj); // 입력값으로 paintToDo 함수 실행
   saveToDo(); // saveToDo 함수 실행
 }
 toDoForm.addEventListener("submit", handleToDoSubmit); // submit 시 hanbleToDoSubmit 함수 실행되는 이벤트 생성
